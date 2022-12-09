967b03779a3a27aa47f478b61b22774f291dc14e
gharynsl123 <persolna1243@gmail.com>
2022-12-09 14:56:13 +0700

    Making Login Register

---
+++ app/src/main/java/com/appteam/muslimeera/ui/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.appteam.muslimeera.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.appteam.muslimeera.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}

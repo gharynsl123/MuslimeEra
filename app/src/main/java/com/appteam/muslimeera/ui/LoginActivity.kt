13864ac0309f3f377133d4cebe67283b64deca23
gharynsl123 <persolna1243@gmail.com>
2022-12-13 08:13:53 +0700

    Continue Login And Register

--- app/src/main/java/com/appteam/muslimeera/ui/LoginActivity.kt
+++ app/src/main/java/com/appteam/muslimeera/ui/LoginActivity.kt
@@ -3,11 +3,15 @@ package com.appteam.muslimeera.ui
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.appteam.muslimeera.R
 import com.appteam.muslimeera.databinding.ActivityLoginBinding
 import com.appteam.muslimeera.ui.main.MainActivity
+import com.jakewharton.rxbinding2.widget.RxTextView
+import io.reactivex.Observable
 
 class LoginActivity : AppCompatActivity() {
-    private var _binding : ActivityLoginBinding? = null
+    private var _binding: ActivityLoginBinding? = null
     private val binding get() = _binding as ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,12 +21,55 @@ class LoginActivity : AppCompatActivity() {
         supportActionBar?.hide()
 
         binding.apply {
+
+            val usernameStream = RxTextView.textChanges(etEmail)
+                .skipInitialValue()
+                .map {
+                    it.isEmpty()
+                }
+            usernameStream.subscribe {
+                textMinimal(it, "userName")
+            }
+            val passwordStream = RxTextView.textChanges(etPass)
+                .skipInitialValue()
+                .map {
+                    it.isEmpty()
+                }
+            passwordStream.subscribe {
+                textMinimal(it, "password")
+            }
+            val invalidStream = Observable.combineLatest(
+                usernameStream,
+                passwordStream,
+            ) { usernameInvalid: Boolean,
+                passwordInvalid: Boolean ->
+                !usernameInvalid && !passwordInvalid
+            }
+            invalidStream.subscribe {
+                if (it) {
+                    llButonLogin.isEnabled = true
+                    llButonLogin.background =
+                        ContextCompat.getDrawable(this@LoginActivity, R.drawable.btn_back_img)
+                } else {
+                    llButonLogin.isEnabled = false
+                }
+            }
+
             llButonLogin.setOnClickListener {
                 startActivity(Intent(this@LoginActivity, MainActivity::class.java))
             }
             tvsigin.setOnClickListener {
-                startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+                startActivity(Intent(this@LoginActivity, RegisterActivity::class.java))
             }
         }
     }
+
+    private fun textMinimal(notValid: Boolean, text: String) {
+        if (text == "Username/Email") {
+            binding.etEmail.error = if (notValid) "$text Tidak Boleh Kosong" else null
+        } else if (text == "password") {
+            binding.etPass.error = if (notValid) "$text Tidak Boleh Kosong" else null
+        }
+    }
+
 }
--- app/src/main/java/com/appteam/muslimeera/ui/RegisterActivity.kt
+++ app/src/main/java/com/appteam/muslimeera/ui/RegisterActivity.kt
@@ -2,11 +2,16 @@ package com.appteam.muslimeera.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Patterns
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.appteam.muslimeera.R
 import com.appteam.muslimeera.databinding.ActivityRegisterBinding
+import com.jakewharton.rxbinding2.widget.RxTextView
+import io.reactivex.Observable
 
 class RegisterActivity : AppCompatActivity() {
-    private var _binding : ActivityRegisterBinding? = null
+    private var _binding: ActivityRegisterBinding? = null
     private val binding get() = _binding as ActivityRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,6 +20,78 @@ class RegisterActivity : AppCompatActivity() {
         supportActionBar?.hide()
 
         binding.apply {
+            val nameStream = RxTextView.textChanges(etFullName)
+                .skipInitialValue()
+                .map {
+                    it.isEmpty()
+                }
+            nameStream.subscribe {
+                showNameExist(it)
+            }
+
+            val emailStream = RxTextView.textChanges(etEmail)
+                .skipInitialValue()
+                .map {
+                    !Patterns.EMAIL_ADDRESS.matcher(it).matches()
+                }
+            emailStream.subscribe {
+                emailValid(it)
+            }
+
+            val usernameStream = RxTextView.textChanges(etUserName)
+                .skipInitialValue()
+                .map {
+                    it.length < 3
+                }
+            usernameStream.subscribe {
+                textMinimal(it, "userName")
+            }
+            val passwordStream = RxTextView.textChanges(etPass)
+                .skipInitialValue()
+                .map {
+                    it.length < 8
+                }
+            passwordStream.subscribe {
+                textMinimal(it, "password")
+            }
+
+            val passwordConfirmStream = Observable.merge(
+                RxTextView.textChanges(etPass)
+                    .skipInitialValue()
+                    .map {
+                        it.toString() != etConfirmPassword.text.toString()
+                    },
+                RxTextView.textChanges(etConfirmPassword)
+                    .skipInitialValue()
+                    .map {
+                        it.toString() != etPass.text.toString()
+                    })
+
+            passwordConfirmStream.subscribe {
+                passwordConfirm(it)
+            }
+
+            val invalidStream = Observable.combineLatest(
+                nameStream,
+                emailStream,
+                usernameStream,
+                passwordStream,
+                passwordConfirmStream
+            ) { nameInvalid: Boolean, emailInvalid: Boolean,
+                usernameInvalid: Boolean,
+                passwordInvalid: Boolean,
+                passwordConfirmInvalid: Boolean ->
+                !nameInvalid && !emailInvalid && !usernameInvalid && !passwordInvalid && !passwordConfirmInvalid
+            }
+            invalidStream.subscribe{
+                if (it){
+                    llButonRegister.isEnabled = true
+                    llButonRegister.background = ContextCompat.getDrawable(this@RegisterActivity, R.drawable.btn_back_img)
+                }else{
+                    llButonRegister.isEnabled = false
+                }
+            }
+
             llButonRegister.setOnClickListener {
                 startActivity(Intent(this@RegisterActivity, LoginActivity::class.java))
             }
@@ -22,5 +99,25 @@ class RegisterActivity : AppCompatActivity() {
                 startActivity(Intent(this@RegisterActivity, LoginActivity::class.java))
             }
         }
+    }
+
+    private fun showNameExist(notValid: Boolean) {
+        binding.etFullName.error = if (notValid) "Nama Tidak Boleh Kosong" else null
+    }
+
+    private fun textMinimal(notValid: Boolean, text: String) {
+        if (text == "Username") {
+            binding.etUserName.error = if (notValid) "$text Harus Lebih Dari 3 huruf" else null
+        } else if (text == "password") {
+            binding.etPass.error = if (notValid) "$text Harus Lebih Dari 8 huruf" else null
+        }
+    }
+
+    private fun emailValid(notValid: Boolean) {
+        binding.etEmail.error = if (notValid) "Email Tidak Valid" else null
+    }
+
+    private fun passwordConfirm(notValid: Boolean) {
+        binding.etConfirmPassword.error = if (notValid) "Password Tidak Sama" else null
     }
 }
